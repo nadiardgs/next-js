229ffae99f922bf724c3903fe7a888fc423e58b2
Nadia Rodrigues <nadia.rodrigues93@gmail.com>
2024-05-30 22:18:43 +0100

    commit

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -22,7 +22,16 @@ test.describe('API add-payment', () => {
 
       const str = json[0].split(':');
       USER.userId = parseInt(str[6]);
-      console.log(USER.userId);
+      console.log(str[0]);
+      console.log(str[1]);
+      console.log(str[2]);
+      console.log(str[3]);
+      console.log(str[4]);
+      console.log(str[5]);
+      console.log(str[6]);
+      console.log(str[7]);
+      console.log(str[8]);
+
     });
 
     test.afterAll(async ({request}) => {
