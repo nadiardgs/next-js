a9f7f065a28c03964208aa79229cead11120a4d4
Nadia Rodrigues <nadia.rodrigues93@gmail.com>
2024-05-30 17:58:05 +0100

    create first test

--- tests/payment/payment-tests.ts
+++ tests/payment/payment-tests.ts
@@ -1,18 +1,7 @@
 import test, { expect } from "@playwright/test";
 
-test('should create a bug report', async ({ request }) => {
-    const newIssue = await request.post(`/add-user`, {
-      data: {
-        userName: 'John Doe',
-        userEmail: 'johndoe@john.com',
-      }
-    });
-    expect(newIssue.ok()).toBeTruthy();
-  
-    const issues = await request.get(`/get-all-users`);
-    expect(issues.ok()).toBeTruthy();
-    expect(await issues.json()).toContainEqual(expect.objectContaining({
-      title: 'John Doe',
-      body: 'johndoe@john.com'
-    }));
+test('test page', async ({ page }) => {
+    const response = await page.goto(`https://next-js-lilac-tau-38.vercel.app/api/add-user`);
+    
+    await expect(page).toBeTruthy();
   });
