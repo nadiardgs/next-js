a4654a9774e7fea70f26f3b907f90da762cbe47b
Nadia Rodrigues <nadia.rodrigues93@gmail.com>
2024-05-30 19:42:07 +0100

    corrections on tests, app-payment should be post, not get

--- tests/payment/payment-tests.ts
+++
@@ -1,7 +0,0 @@
-import test, { expect } from "@playwright/test";
-
-test('test page', async ({ page }) => {
-    const response = await page.goto(`https://next-js-lilac-tau-38.vercel.app/api/add-user`);
-    
-    await expect(page).toBeTruthy();
-  });
